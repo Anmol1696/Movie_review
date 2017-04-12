a2a5c8a9a692f62fc5166d95db06639d64b9a025
Anmol1696 <anmol1696@gmail.com>
2017-04-12 19:01:18 +0530

    Helping functions for pandas data frames

--- form_data/pandas_functions.py
+++ form_data/pandas_functions.py
@@ -2,6 +2,7 @@
     Consists of all the functions for pandas
 """
 
+import sys
 import pandas
 
 def read_pandas_raw_csv(file_name):
@@ -12,3 +13,28 @@ def read_pandas_raw_csv(file_name):
 
     return data_frame
 
+def form_sentiment_vs_file_name_data_frame(data_frame, write_to_file):
+    """
+        Given the raw data frame as the input form another data frame with index as the sentiment and column as the file_name
+        For getting all files with pos sentiment use `sentiment_data_frame.pos`
+    """
+    temp_dict = {'neg' : [], 'pos' : []}
+
+    for file_name in data_frame.index:
+        sentiment = data_frame.get_value(index=file_name, col='sentiment')
+        temp_dict[str(sentiment)].append(file_name)
+
+    sentiment_data_frame = pandas.DataFrame(temp_dict)
+
+    if write_to_file:
+        sentiment_data_frame.to_csv(write_to_file)
+
+if __name__ == "__main__":
+    try:
+        raw_file_name = sys.argv[1]
+        sentiment_file_name = sys.argv[2]
+    except:
+        print '[ERROR] in input file'
+
+    data_frame = read_pandas_raw_csv(raw_file_name)
+    form_sentiment_vs_file_name_data_frame(data_frame, sentiment_file_name)
