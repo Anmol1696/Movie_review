5414e1d469af6879ac573d526a25a22efccaa918
Anmol1696 <anmol1696@gmail.com>
2017-04-13 05:51:14 +0530

    Modified the code, Working for small examples, just have to scale it

--- classification/naviebayes_classification.py
+++ classification/naviebayes_classification.py
@@ -8,32 +8,48 @@ from sklearn.naive_bayes                import MultinomialNB
 from sklearn.feature_extraction.text    import CountVectorizer
 
 import sys
+import pandas
 
-def classify_NB(data_frame):
-    count_vectorizer = CountVectorizer()
-    counts = count_vectorizer.fit_transform(data_frame['processed_words'].values)
-    
+count_vectorizer = CountVectorizer()
+
+def classify_NB(train_data_frame):
+    counts = count_vectorizer.fit_transform(train_data_frame['processed_words'].values)    
     classifier = MultinomialNB()
-    targets = data_frame['sentiment'].values
+    targets = train_data_frame['sentiment'].values
     classifier.fit(counts, targets)
 
     return classifier
 
-def check_all_from_same_data_frame(classifier, data_frame):
-    count_vectorizer = CountVectorizer()
-    example_counts = count_vectorizer.transform(data_frame['processed_words'].values)
+def append_prediction_to_data_frame(classifier, test_data_frame):
+    """
+        Get predictions from MultinomialNB and append to the processed data frame
+    """
+    example_counts = count_vectorizer.transform(test_data_frame['processed_words'].values)
     predictions = classifier.predict(example_counts)
-    
-    for i in range(len(predictions)):
-        print 'Pridictions -> ', predictions[i], ' | Actual -> ', data_frame['sentiment'].values[i]
+    test_data_frame['prediction'] = map(unicode, predictions)
 
+    return test_data_frame
+
+def main_classification(train_data_file_name, test_data_file_name, write_back_test_data):
+    train_data_frame = read_pandas_raw_csv(train_data_file_name)
+    test_data_frame = read_pandas_raw_csv(test_data_file_name)
+
+    classifier = classify_NB(train_data_frame)
+
+    test_data_frame = append_prediction_to_data_frame(classifier, test_data_frame)
+
+    if write_back_test_data == "True":
+        test_data_frame.to_csv(test_data_file_name)
+
+    return test_data_frame
 
 if __name__ == "__main__":
     try:
-        data_file_name = sys.argv[1]
+        train_data_file_name = sys.argv[1]
+        test_data_file_name = sys.argv[2]
+        write_back_test_data = sys.argv[3]
     except:
         print '[ERROR] in input'
 
-    data_frame = read_pandas_raw_csv(data_file_name)
-    c = classify_NB(data_frame)
-    check_all_from_same_data_frame(c, data_frame)
+    t = main_classification(train_data_file_name, test_data_file_name, write_back_test_data)
+    print 'Test Data Frame ->\n', t
